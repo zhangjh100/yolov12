b4e02576738355008f86d00a2e53c768a054dff9
zhangjh100 <zhangjh100@nenu.edu.cn>
2025-11-03 14:46:52 +0800

    update 1

--- ultralytics/models/yolo/segment/val.py
+++ ultralytics/models/yolo/segment/val.py
@@ -54,7 +54,7 @@ class SegmentationValidator(DetectionValidator):
 
     def get_desc(self):
         """Return a formatted description of evaluation metrics."""
-        return ("%22s" + "%11s" * 10) % (
+        return ("%22s" + "%11s" * 12) % (
             "Class",
             "Images",
             "Instances",
