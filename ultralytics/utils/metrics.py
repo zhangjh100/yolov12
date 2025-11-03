8affba8e3d989336ea7ce8e9498bf7e6ecf60b30
zhangjh100 <zhangjh100@nenu.edu.cn>
2025-11-03 16:22:21 +0800

    update 1

--- ultralytics/utils/metrics.py
+++ ultralytics/utils/metrics.py
@@ -1049,8 +1049,8 @@ class SegmentMetrics(SimpleClass):
         valid_iou = [x for x in self.iou if isinstance(x, torch.Tensor)]
         if not valid_iou:
             return 0.0
-        # return torch.cat(self.iou).mean().item()
-        return torch.cat(valid_iou).mean().item()
+        return torch.cat(self.iou).mean().item()
+        # return torch.cat(valid_iou).mean().item()
 
     @property
     def mean_dice(self):
